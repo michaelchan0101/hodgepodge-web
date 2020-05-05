16cf6a712dff2db4fa904fe5fb0a3f5aaa80bade
zn <312073339@qq.com>
2020-05-05 19:25:42 +0800

    feat: add logger

---
+++ project/server/src/utils/logger.ts
@@ -0,0 +1,21 @@
+import winston from 'winston'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const debugFormat = winston.format.printf(info => {
+  return `${info.timestamp} ${info.level}: ${info.message}`
+})
+
+const logger = winston.createLogger({
+  level: isDev ? 'debug' : 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.splat(),
+    winston.format.json(),
+    ...(isDev ? [winston.format.colorize(), debugFormat] : [winston.format.json()])
+  ),
+  // defaultMeta: { service: 'project-service' },
+  transports: [new winston.transports.Console()],
+})
+
+export default logger
