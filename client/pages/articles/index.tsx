476868548c2d8e0a17bc851d92d3f7e9173f071c
michaelchan0101 <312073339@qq.com>
2020-06-02 18:32:40 +0800

    feat(client): article style

--- client/pages/articles/index.tsx
+++ client/pages/articles/index.tsx
@@ -1,5 +1,7 @@
+import style from 'styles/article.module.css'
 import { listArticles } from 'services/article'
 import { ArticlesProps } from 'types/article'
+import Link from 'next/link'
 
 export async function getStaticProps() {
   const [limit, offset] = [20, 0]
@@ -13,8 +15,23 @@ export async function getStaticProps() {
 
 export default function Articles(props: ArticlesProps) {
   return (
-    <div>
-      <h1 className='title'>Welcome to Articles</h1>
-    </div>
+    <>
+      <div className={style.articleHeader}>文章列表</div>
+      <ul className={style.articleBox}>
+        <li>
+          <Link href='/detail/1'>
+            <a>文章标题</a>
+          </Link>
+          <span>2020-03-01 10:30:00</span>
+        </li>
+        <li>
+          <a>文章标题</a>
+          <span>2020-03-01 10:30:00</span>
+        </li>
+        <li>
+          <a>获取更多文章...</a>
+        </li>
+      </ul>
+    </>
   )
 }
