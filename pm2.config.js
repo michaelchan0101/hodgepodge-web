5a2f4e48e5fabd2b8b20e60c499b5a3af5b216f6
michaelchan0101 <312073339@qq.com>
2020-06-15 11:26:56 +0800

    feat: add `hodgepodge_client` in pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -25,5 +25,30 @@ module.exports = {
         NODE_HODGEPODGE_PORT: '3000',
       },
     },
+    {
+      name: 'hodgepodge_client',
+      instances: '1',
+      exec_mode: 'cluster',
+      script: './server/dist/client.js',
+      merge_logs: true,
+      restart_delay: 5000,
+      wait_ready: true,
+      env: {
+        watch: false,
+        autorestart: true,
+      },
+      env_production: {
+        NODE_HODGEPODGE_ENV: 'production',
+        NODE_HODGEPODGE_PORT: '3001',
+      },
+      env_staging: {
+        NODE_HODGEPODGE_ENV: 'staging',
+        NODE_HODGEPODGE_PORT: '3001',
+      },
+      env_development: {
+        NODE_HODGEPODGE_ENV: 'development',
+        NODE_HODGEPODGE_PORT: '3001',
+      },
+    },
   ],
 }
