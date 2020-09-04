1e9f4a9bd130502039b59605e9d84e2306e2e3fe
michaelchan0101 <312073339@qq.com>
2020-09-04 12:28:46 +0800

    fix: base url

--- src/apis/article.ts
+++ src/apis/article.ts
@@ -5,11 +5,13 @@ export async function listArticles(
   limit: number,
   offset: number
 ): Promise<ListArticlesResponse> {
-  const result = await fetch.get('/api/v1.0/articles', { params: { limit, offset } })
+  const result = await fetch.get('/api/client/v1.0/articles', {
+    params: { limit, offset },
+  })
   return result.data
 }
 
 export async function getArticle(id: number): Promise<ArticleResponse> {
-  const result = await fetch.get(`/api/v1.0/articles/${id}`)
+  const result = await fetch.get(`/api/client/v1.0/articles/${id}`)
   return result.data
 }
